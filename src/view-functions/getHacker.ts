7f5acd83d6d4c2d7f23633604fc28b9ebb768b59
Aditya Pundir <pundir.aditya@outlook.com>
2024-12-02 04:46:14 +0530

    main pull

--- src/view-functions/getHacker.ts
+++ src/view-functions/getHacker.ts
@@ -4,15 +4,15 @@ export const getHacker = async (address: `0x${string}`) => {
   try {
     console.log("get hackers fxn run");
     console.log("Contract Address", process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
-    const hackers = await aptosClient().view({
+    const hacker = await aptosClient().view({
       payload: {
         function: `${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}::HackerOneCore::getHacker`,
         typeArguments: [],
         functionArguments: [address],
       },
     });
-    console.log(hackers[0]);
-    if (hackers[0]) return hackers[0];
+    console.log(hacker[0]);
+    if (hacker[0]) return hacker[0];
     else return null;
   } catch (error: any) {
     if (error.message.includes("112")) {
