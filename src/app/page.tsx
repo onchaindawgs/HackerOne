65f9be42b8a0c8b31c60ad48e8fbd098de2d7982
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-12-02 09:19:46 +0530

    fixed: logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,10 @@
 import HomeHero from "@/components/home/home-hero";
 import { Header, PageContainer } from "@/components/layout";
 
-import { BuildType, OktoProvider } from "okto-sdk-react";
-
-const OKTO_CLIENT_API_KEY = process.env.NEXT_PUBLIC_OKTO_CLIENT_API_KEY as string;
-console.log({ OKTO_CLIENT_API_KEY });
 export default function Home() {
   return (
-    <OktoProvider apiKey={OKTO_CLIENT_API_KEY} buildType={BuildType.SANDBOX}>
-      <PageContainer header={<Header />}>
-        <HomeHero />
-      </PageContainer>
-    </OktoProvider>
+    <PageContainer header={<Header />}>
+      <HomeHero />
+    </PageContainer>
   );
 }
