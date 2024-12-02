98ec79974313a0d13a19c6ebd228471c0c17f1b9
Shashank Mishra <shashank.mishra.dev14@gmail.com>
2024-12-02 11:47:27 +0530

    added vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,5 @@ export default withPWA({
     unoptimized: true
   },
   output: "standalone", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
 });
